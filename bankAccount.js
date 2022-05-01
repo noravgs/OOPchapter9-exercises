c8101a0077b1735cc8d95d01a4dd2974ae9fff36
Nora Vargas <101680775+noravgs@users.noreply.github.com>
2022-05-01 14:53:24 -0500

    created bank account js
    The code is still not returning the right values.

---
+++ bankAccount.js
@@ -0,0 +1,22 @@
+class BankAccount {
+  constructor(name, balance, credit) {
+    this.name = name;
+    this.balance = balance;
+    this.credit = credit;
+    this.sb = 0;
+  }
+  this credit = += this.balance;
+  
+  }
+   describe(){
+    return `owner: ${this.name}, balance: ${this.balance}`;
+}
+}
+
+const sean = new BankAccount ("Sean", 100, 0);
+const brad = new BankAccount ("Brad", 1000, 0);
+const georges = new BankAccount ("Georges", 1000, 0);
+
+console.log(sean.describe());
+console.log(brad.describe());
+console.log(georges.describe());
